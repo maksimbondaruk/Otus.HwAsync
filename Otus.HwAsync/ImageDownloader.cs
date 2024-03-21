00d8657fa0df3cbce1490f8c31ebd28fa80fdeb1
Максим Бондарук <mbondaruk@yandex.ru>
2024-03-21 15:50:23 +0500

    fixed List feeding

--- Otus.HwAsync/ImageDownloader.cs
+++ Otus.HwAsync/ImageDownloader.cs
@@ -6,7 +6,7 @@ namespace Otus.HwAsync
         public event Action<string> DonwloadStarted;
         public event Action<string, long> DonwloadCompleted;
         private string remoteUrl;
-        private string fileName;
+        public string fileName;
         private string path = @"C:\Users\bondaruk\source\repos\Otus.HwAsync\";
        public ImageDownloader(string remoteUrl, string fileName)
         {
@@ -18,13 +18,11 @@ namespace Otus.HwAsync
         HttpClient client = new HttpClient();
         public async Task GetPicture(CancellationToken token)
         {
-            //Console.WriteLine($"Качаю {fileName} из {remoteUrl}");
             var _response = await client.GetAsync(remoteUrl);
             if (_response.IsSuccessStatusCode)
             {
                 this.DonwloadStarted?.Invoke(this.fileName);
                 var _bytes = await _response.Content.ReadAsByteArrayAsync();
-                //await File.WriteAllBytesAsync(path, _bytes);
                 _ = Task.Run(() => File.WriteAllBytesAsync(path, _bytes),token);
 
                 if (token.IsCancellationRequested)
@@ -32,8 +30,6 @@ namespace Otus.HwAsync
 
                 FileInfo fileInfo = new FileInfo(path);
                 this.DonwloadCompleted?.Invoke(this.fileName, fileInfo.Length);
-                //Console.WriteLine($"Успешно скачал {fileName} из {remoteUrl}");
-                Console.WriteLine("WriteAllBytesAsync ID" + Thread.CurrentThread.ManagedThreadId.ToString());
             }
         }
     }
--- Otus.HwAsync/Program.cs
+++ Otus.HwAsync/Program.cs
@@ -22,29 +22,22 @@ namespace Otus.HwAsync
                 eventHandlerList[i].Subscribe(imageDownloaderList[i]);
             }
 
-            //var imageDownloader = new ImageDownloader("https://speedtest.selectel.ru/100MB", "100Mb.txt");
-            //var eventHandler = new EventHandler();
-            //eventHandler.Subscribe(imageDownloader);
             CancellationTokenSource cts = new CancellationTokenSource();
             CancellationToken token = cts.Token;
 
             try
             {
-                //Task t = imageDownloader.GetPicture();
-                //Task task = new(async () => await imageDownloader.GetPicture(token));
                 for (int i = 0; i < Qty; i++)
                 {
-                    doownloaserTaskList[i] = imageDownloaderList[i].GetPicture(token);
+                    doownloaserTaskList.Add(imageDownloaderList[i].GetPicture(token));
                 }
-                //Task task = imageDownloader.GetPicture(token);
                 Console.WriteLine("Нажмите клавишу A для выхода или любую другую клавишу для проверки статуса скачивания");
                 while (Console.ReadLine() != "A")
                 {
                     for(int i = 0; i < Qty; i++)
                     {
-                        Console.WriteLine($"Статус скачивания {doownloaserTaskList[i].Status}");
+                        Console.WriteLine($"Статус скачивания {imageDownloaderList[i].fileName}  {doownloaserTaskList[i].Status}");
                     }
-                    //Console.WriteLine($"Статус скачивания {task.Status}");
                     for (int i = 0; i< Qty; i++)
                     {
                         if ((doownloaserTaskList[i].Status == TaskStatus.RanToCompletion) || 
@@ -58,10 +51,7 @@ namespace Otus.HwAsync
                     {
                         break; 
                     }
-                    //if ((task.Status == TaskStatus.RanToCompletion)||(task.Status == TaskStatus.Faulted)||(task.Status == TaskStatus.Canceled))
-                    //    break;
                     Console.WriteLine("Нажмите клавишу A для выхода или любую другую клавишу для проверки статуса скачивания");
-                    Console.WriteLine("while ID" + Thread.CurrentThread.ManagedThreadId.ToString());
                 }
                 cts.Cancel();
                 for (int i = 0; i < Qty; i++)
