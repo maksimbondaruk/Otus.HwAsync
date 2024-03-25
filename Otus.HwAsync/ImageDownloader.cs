8090eeec79b94c19a222469b40ed383ca244c444
Максим Бондарук <mbondaruk@yandex.ru>
2024-03-25 18:52:50 +0500

    remarks correction

--- Otus.HwAsync/ImageDownloader.cs
+++ Otus.HwAsync/ImageDownloader.cs
@@ -1,13 +1,14 @@
 ﻿
 namespace Otus.HwAsync
 {
-    internal class ImageDownloader
+    internal class ImageDownloader: IDisposable
     {
-        public event Action<string> DonwloadStarted;
-        public event Action<string, long> DonwloadCompleted;
-        private string remoteUrl;
-        public string fileName;
-        private string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        public event Action<string>? DonwloadStarted;
+        public event Action<string, long>? DonwloadCompleted;
+        private readonly string remoteUrl;
+        public readonly string fileName;
+        private readonly string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        private bool disposed = false;
 
         public ImageDownloader(string remoteUrl, string fileName)
         {
@@ -16,22 +17,29 @@ namespace Otus.HwAsync
             this.path += fileName;
         }
 
-        HttpClient client = new HttpClient();
+        readonly HttpClient client = new HttpClient();
         public async Task GetPicture(CancellationToken token)
         {
-            var _response = await client.GetAsync(remoteUrl);
+            var _response = await client.GetAsync(remoteUrl, token);
             if (_response.IsSuccessStatusCode)
             {
                 this.DonwloadStarted?.Invoke(this.fileName);
-                var _bytes = await _response.Content.ReadAsByteArrayAsync();
-                _ = Task.Run(() => File.WriteAllBytesAsync(path, _bytes),token);
-
+                var _bytes = await _response.Content.ReadAsByteArrayAsync(token);
+                await File.WriteAllBytesAsync(path, _bytes);
                 if (token.IsCancellationRequested)
                     token.ThrowIfCancellationRequested(); // генерируем исключение
 
                 FileInfo fileInfo = new FileInfo(path);
                 this.DonwloadCompleted?.Invoke(this.fileName, fileInfo.Length);
             }
+        }
+        
+        public void Dispose()
+        {
+            // освобождаем неуправляемые ресурсы
+            client.Dispose();
+            // подавляем финализацию
+            GC.SuppressFinalize(this);
         }
     }
 }
